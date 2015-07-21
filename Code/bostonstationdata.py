80f3df8cfb5b76687c49113fb14ad7646cb55678
Shane Bussmann <sbussmann@gmail.com>
2015-07-21 10:44:15 -0400

    Station to station scores
    Compute population density score and employee density score as average
    of scores of nearby stations.

---
+++ Code/bostonstationdata.py
@@ -0,0 +1,59 @@
+
+# coding: utf-8
+
+# # Aim is to get zip code for each station for each day
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import densitymetric
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+# skip first 4 cells and just read in the csv files
+population = pd.read_csv('../Data/Boston/population.csv')
+employee = pd.read_csv('../Data/Boston/employee.csv')
+station = pd.read_csv('../Data/Boston/hubway_stations.csv')
+
+# Generate a regular grid to interpolate the data.
+xmin = 42.29
+xmax = 42.43
+ymin = -71.19
+ymax = -70.98
+nx = 200
+ny = 200
+xvec = np.linspace(xmin, xmax, nx)
+yvec = np.linspace(ymin, ymax, ny)
+xi, yi = np.meshgrid(xvec, yvec)
+x = employee['latitude']
+y = employee['longitude']
+z = employee['EMP']
+workmap = mlab.griddata(x, y, z, xi, yi, interp='linear')
+x = population['latitude']
+y = population['longitude']
+z = population['SUBHD0401']
+popmap = mlab.griddata(x, y, z, xi, yi, interp='linear')
+#origdest = origin * destination
+
+
+# In[3]:
+
+stationlat = station['lat'].values
+stationlong = station['lng'].values
+closeradius = 0.25
+scaledistance = 1.0
+scores = densitymetric.getscores(xvec, yvec, popmap, workmap, stationlat, 
+        stationlong, closeradius, scaledistance)
+
+#plt.hist(scores[0], alpha=0.5)
+#plt.hist(scores[1], alpha=0.5)
+plt.scatter(scores[0], scores[1])
+plt.xlabel('Population density')
+plt.ylabel('Employee density')
+plt.show()
+
+import pdb; pdb.set_trace()
--- Code/densitymetric.py
+++ Code/densitymetric.py
@@ -1,33 +1,124 @@
-
-# coding: utf-8
-
-"""
-
-Measure density within "radius" of "latitude, longitude"
-
-Output: Quantitative measure of origin / destination density.
-
-"""
-
 import numpy as np
 from geopy.distance import vincenty
 
 
-def getval(xmap, ymap, popmap, workmap, inlat, inlong, radius):
+def distmap(latvec, longvec, inlat, inlong):
+
+    """
+
+    Map the distance in miles from a given latitude and longitude.
+    
+    Input: 
+        xvec: high resolution vector of latitudes [numpy array]
+        yvec: high resolution vector of longitudes [numpy array]
+        inlat: latitude of station [float]
+        inlong: longitude of station [float]
+
+    Output: 
+        distancemap: distance in miles from (inlat, inlong) [numpy array]
+
+    """
+
 
     origin = (inlat, inlong)
-    nlat = len(workmap[:, 0])
-    nlong = len(workmap[0, :])
-    distance = np.zeros([nlat, nlong])
+    nlat = len(latvec)
+    nlong = len(longvec)
+    distancemap = np.zeros([nlat, nlong])
     for ilat in range(nlat):
         for ilong in range(nlong):
-            destination = (xmap[ilat], ymap[ilong])
-            distance[ilat, ilong] = vincenty(origin, destination).miles
-    close = np.where(distance < 2.0)
+            destination = (latvec[ilat], longvec[ilong])
+            distancemap[ilat, ilong] = vincenty(origin, destination).miles
 
-    popmeasure = np.median(popmap[close])
-    workmeasure = np.median(workmap[close])
-    measure = (popmeasure, workmeasure)
-    #print(measure)
+    return distancemap
 
-    return measure
+def coupling(latvec, longvec, distancemap, stationlat, stationlong,
+        scaledistance):
+
+    """
+
+    Compute the coupling efficiency between a given station and all other
+    stations.  Model coupling efficiency with a Gaussian.
+
+    Inputs: 
+        stationlat: station latitudes [list]
+        stationlong: station longitudes [list]
+        distancemap: map of distance from given station to all other points
+        in Greater Boston area [numpy array]
+        scaledistance: length scale over which coupling efficiency is expected
+        to decrease by 1/e [float]
+
+    Outputs:
+        eta_coupling: coupling efficiencies for each station [list]
+
+    """
+
+    from scipy.interpolate import interp2d
+
+    sdfunc = interp2d(latvec, longvec, distancemap, kind='cubic')
+    stationdistance = []
+    for i in range(len(stationlat)):
+        interp = sdfunc(stationlat[i], stationlong[i])
+        stationdistance.append(interp[0])
+    stationdistance = np.array(stationdistance)
+    stationcoupling = np.exp(-0.5 * (stationdistance / scaledistance) ** 2)
+
+    return stationcoupling
+
+def getscores(latvec, longvec, popmap, workmap, stationlat, stationlong,
+        closeradius, scaledistance):
+
+    """
+
+    Compute the metrics for population density and employee density.
+
+    TO DO: add MBTA T stop data.
+
+    inputs:
+        latvec: vector of high resolution latitudes [numpy array]
+        longvec: vector of high resolution longitudes [numpy array]
+        popmap: high resolution map of population density [numpy array]
+        workmap: high resolution map of employee density [numpy array]
+        stationcoupling: coupling efficiency
+
+    """
+
+    nstation = len(stationlat)
+    popmeasures = []
+    workmeasures = []
+    stationcoupling = np.zeros([nstation, nstation])
+    for i in range(nstation):
+        inlat = stationlat[i]
+        inlong = stationlong[i]
+        distancemap = distmap(latvec, longvec, inlat, inlong)
+        close = distancemap < closeradius
+        popmeasures.append(popmap[close].mean())
+        workmeasures.append(workmap[close].mean())
+        stationcoupling[i, :] = coupling(latvec, longvec, distancemap, 
+                stationlat, stationlong, scaledistance)
+        values = (i, popmeasures[i], workmeasures[i], 
+                stationcoupling[i, :].min(), stationcoupling[i, :].max(),
+                stationcoupling[i, :].mean())
+        fmt = '{0} {1:.2f} {2:.2f} {3:.5f} {4:.3f} {5:.3f}'
+        print(fmt.format(values))
+
+    # test: Is there a station in stationcoupling that is ~1?  Are stations
+    # that are known to be far from each other correctly assigned a low
+    # coupling efficiency?
+
+    #import pdb; pdb.set_trace()
+
+
+    popscore = []
+    workscore = []
+    for i in range(nstation):
+        popscore.append(np.average(popmeasures, 
+            weights=stationcoupling[i, :]))
+        workscore.append(np.average(workmeasures, 
+            weights=stationcoupling[i, :]))
+
+    import pdb; pdb.set_trace()
+    scores = [popscore, workscore]
+    return scores
+
+#station['popdensity'] = popdensity
+#station['empdensity'] = empdensity
