8296659a3476559b2c8730aaf44bfb8b875ca37c
Shane Bussmann <sbussmann@gmail.com>
2015-07-28 11:03:08 -0400

    Debug AWS
    Trying to solve the following error: Safari can’t connect to the server.

--- Code/views.py
+++ Code/views.py
@@ -1,26 +1,26 @@
 from flask import render_template, request, Flask
-#from webapp import webapp
+#from app import app
 #import pymysql as mdb
 #from predictride import predict
 import gridpredict
 
-webapp = Flask(__name__)
+app = Flask(__name__)
 
 #db = mdb.connect(user="root", host="localhost", password="password",
 #        db="BostonFeaturesByStation_db", charset='utf8')
 
-@webapp.route('/')
-@webapp.route('/index')
+@app.route('/')
+@app.route('/index')
 def index():
 	return render_template("index.html",
         title = 'Home', user = { 'nickname': 'Shane' },
         )
 
-@webapp.route('/input')
+@app.route('/input')
 def station_input():
   return render_template("input.html")
 
-@webapp.route('/output-auto')
+@app.route('/output-auto')
 def station_output_auto():
   iterstring = '0'
   the_results = gridpredict.autoinput(iterstring)
@@ -29,7 +29,7 @@ def station_output_auto():
   iterstring = the_results[2]
   return render_template("output.html", riderate=riderate, ranking=ranking)
 
-@webapp.route('/output-user')
+@app.route('/output-user')
 def station_output_user():
   #pull 'ID' from input field and store it
   longitude = request.args.get('ID1')
@@ -44,4 +44,4 @@ def station_output_user():
   return render_template("output.html", riderate=riderate, ranking=ranking)
 
 if __name__ == "__main__":
-   webapp.run(host='0.0.0.0', port=5000, debug=True)
+   app.run(host='0.0.0.0', port=5000, debug=True)
