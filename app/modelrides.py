43eb4ef94694c348fcbca65a4ec972c577683c88
Shane Bussmann <sbussmann@gmail.com>
2015-08-06 15:58:42 -0400

    Explore correlated features
    Destwork and Destsubway are highly correlated.  Replacing these with
    Destwork+Destsubway and Destwork-Destsubway reduce the collinearity.
    Nevertheless, the features remain highly correlated in complex ways.
    Also, random forests don’t give any better of a fit to the data.

--- app/modelrides.py
+++ app/modelrides.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import PolynomialFeatures
 #from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn import tree
-#from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 
 
@@ -24,57 +24,82 @@ y = ordinary['ridesperday'].values
 
 X = ordinary[['originpop', 'originwork', 'originsubway',
 'destpop', 'destwork', 'destsubway']].values
+X_original = ordinary[['originpop', 'originwork', 'originsubway',
+'destpop', 'destwork', 'destsubway']].values
+
+X = preprocessing.scale(X)
+tmp4 = X[:,4]
+tmp5 = X[:,5]
+X[:, 4] = (tmp4 + tmp5) / 2.
+X[:, 5] = (tmp4 - tmp5) / 2.
+X = preprocessing.scale(X)
+X_scaled = X.copy()
+
+ordinary['originpop'] = X[:, 0]
+ordinary['originwork'] = X[:, 1]
+ordinary['originsubway'] = X[:, 2]
+ordinary['destpop'] = X[:, 3]
+ordinary['destwork'] = X[:, 4]
+ordinary['destsubway'] = X[:, 5]
 
 #X = np.exp(X/100)
-X_scaled = preprocessing.scale(X)
+
+def niceprint(modeltype, metric, scores):
+    smean = str(round(scores.mean(), 2))
+    srms = str(round(scores.std(), 2))
+    print(modeltype + ", " + metric + ': %s +/- %s' % (smean, srms))
+    return
 
 # use linear regression
 clf = linear_model.LinearRegression()
 
+metric = "mean_absolute_error"
+
 # compute r2 score using 5-fold cross-validation
 scaledscores = cross_validation.cross_val_score(clf, X_scaled, y, cv=5,
-        scoring='mean_absolute_error')
-scores = cross_validation.cross_val_score(clf, X, y, cv=5,
-        scoring='median_absolute_error')
-
-print("Scores for scaled linear regression: \n", 
-        scaledscores, scaledscores.mean(), scaledscores.std())
-print("Scores for linear regression: \n", 
-        scores, scores.mean(), scores.std())
+        scoring=metric)
+scores = cross_validation.cross_val_score(clf, X_original, y, cv=5,
+        scoring=metric)
+niceprint('Linear Regression', metric, scores)
 
 clf = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0])
-scores = cross_validation.cross_val_score(clf, X_scaled, y, cv=5, scoring='r2')
-print("Scores for Ridge regression: \n", 
-        scores, scores.mean(), scores.std())
-clf.fit(X_scaled, y)       
-print(clf.alpha_, clf.coef_)
+scores = cross_validation.cross_val_score(clf, X_scaled, y, cv=5,
+        scoring=metric)
+niceprint('Ridge Regression', metric, scores)
+#clf.fit(X_scaled, y)       
+#print(clf.alpha_, clf.coef_)
 
-polynomial_features = PolynomialFeatures(degree=2)
+polynomial_features = PolynomialFeatures(degree=3)
 ridge_regression = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0])
 pipeline = Pipeline([("polynomial_features", polynomial_features),
                      ("ridge_regression", ridge_regression)])
-pipeline.fit(X_scaled, y)
+#pipeline.fit(X_scaled, y)
 
 # Evaluate the models using crossvalidation
 scores = cross_validation.cross_val_score(pipeline,
-    X_scaled, y, scoring="mean_squared_error", cv=5)
+    X_scaled, y, scoring=metric, cv=5)
 
-print("Scores for Polynomial regression: \n", 
-        scores, scores.mean(), scores.std())
+niceprint("Polynomial Ridge regression", metric, scores)
 
 clf = tree.DecisionTreeRegressor()
 clf = clf.fit(X, y)
-scores = cross_validation.cross_val_score(clf, X, y, cv=5, scoring='r2')
-print("Scores for Decision Tree regression: \n", 
-        scores, scores.mean(), scores.std())
+scores = cross_validation.cross_val_score(clf, X, y, cv=5,
+        scoring=metric)
+niceprint("Decision Tree regression", metric, scores)
 
-#clf = RandomForestRegressor(n_estimators=1000)
-#clf = clf.fit(X, y)
-#scores = cross_validation.cross_val_score(clf, X, y, cv=5, scoring='r2')
-#print("Scores for Random Forest regression: \n", 
-#        scores, scores.mean(), scores.std())
-#clf.fit(X, y)       
+clf = RandomForestRegressor(n_estimators=100, max_features='sqrt')
+clf = clf.fit(X, y)
 #print(clf.alpha_, clf.coef_)
+scores = cross_validation.cross_val_score(clf, X, y, cv=5, scoring=metric)
+niceprint("Random Forest Regression:", metric, scores)
+
+clf = RandomForestRegressor(n_estimators=10000)
+poly = PolynomialFeatures(degree=4)
+Xpoly = poly.fit_transform(X)
+clf = clf.fit(Xpoly, y)
+scores = cross_validation.cross_val_score(clf, Xpoly, y, cv=5, scoring=metric)
+niceprint("Random Forest Polynomial Regression:", metric, scores)
+#clf.fit(X, y)       
 #print("Coefficients for Polynomial regression: ")
 #model = Pipeline([('poly', PolynomialFeatures(degree=3)), 
 #    ('linear', LinearRegression())])
@@ -95,7 +120,7 @@ for isim in range(10):
     plt.xlabel('True Daily Rides', fontsize='xx-large')
     plt.ylabel('Predicted Daily Rides', fontsize='xx-large')
     plt.tight_layout()
-    print(clf.coef_)
+    #print(clf.coef_)
     modelcoef.append(clf.coef_)
 
 modelcoef = np.array(modelcoef)
@@ -105,13 +130,16 @@ print(modelcoef.mean(axis=0))
 savefig('../Figures/ridesperdayregression.png')
 
 # plot correlations between features
-#import seaborn as sns
-#plt.clf()
-#sns.set(style="ticks", color_codes=True)
-#ordinaryfeat = ordinary[['originpop', 'originwork', 'destpop', 'destwork',
-#'originsubway', 'destsubway', 'ridesperday']]
-#ordinaryfeat = ordinaryfeat.sort('ridesperday')
-#g = sns.pairplot(ordinaryfeat, hue="ridesperday", palette="Blues", size=4)
-#plt.tight_layout()
-#savefig('../Figures/FeatureCorrelation.png')
+import seaborn as sns
+plt.clf()
+sns.set(style="ticks", color_codes=True)
+ordinaryfeat = ordinary[['originsubway', 'destpop', 'destwork', 'destsubway', 'ridesperday']]
+#tmp1 = ordinary['destsubway'] + ordinary['destwork']
+#tmp2 = ordinary['destsubway'] - ordinary['destwork']
+#ordinaryfeat['destsubpluswork'] = tmp1
+#ordinaryfeat['destsubminuswork'] = tmp2
+ordinaryfeat = ordinaryfeat.sort('ridesperday')
+g = sns.pairplot(ordinaryfeat, hue="ridesperday", palette="Blues", size=4)
+plt.tight_layout()
+savefig('../Figures/FeatureCorrelation.png')
 import pdb; pdb.set_trace()
